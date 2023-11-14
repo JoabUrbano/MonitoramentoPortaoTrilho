967aff365d85989b24abdc72ced76f2b38a6b1b4
Joab Urbano <joab.uaraujo@gmail.com>
2023-11-14 19:47:51 -0300

    Conexão Wifi

--- MonitoramentoPortaoTrilho.ino
+++ MonitoramentoPortaoTrilho.ino
@@ -1,7 +1,23 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+#include <NTPClient.h> // https://github.com/arduino-libraries/NTPClient
+
 #define LED 2
 #define Sensor01 4
 #define Sensor02 32
 #define Sensor03 33
+
+#define wifi_ssid "imd0902"
+#define wifi_password "imd0902iot"
+
+int wifi_timeout = 100000;
+
+WiFiUDP udp;
+NTPClient ntp(udp, "a.st1.ntp.br", -3 * 3600, 60000); // Cria um objeto "NTP" com as configurações.utilizada no Brasil
+String hora;
+
+void connectWiFi();
 
 void setup()
 {
@@ -10,7 +26,13 @@ void setup()
     pinMode(Sensor03, INPUT);
     pinMode(LED, OUTPUT);
     Serial.begin(115200);
-    Serial.println();
+
+    WiFi.mode(WIFI_STA); //"station mode": permite o ESP32 ser um cliente da rede WiFi
+    WiFi.begin(wifi_ssid, wifi_password);
+    connectWiFi();
+
+    ntp.begin();       // Inicia o protocolo
+    ntp.forceUpdate(); // Atualização
 }
 void loop()
 {
@@ -26,5 +48,29 @@ void loop()
     else
     {
         digitalWrite(LED, LOW);
+    }
+}
+
+// Função para conectar o Esp ao Wifi
+void connectWiFi()
+{
+    Serial.print("Conectando à rede WiFi .. ");
+
+    unsigned long tempoInicial = millis();
+    while (WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout))
+    {
+        Serial.print(".");
+        delay(100);
+    }
+    Serial.println();
+
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.println("Conexão com WiFi falhou!");
+    }
+    else
+    {
+        Serial.print("Conectado com o IP: ");
+        Serial.println(WiFi.localIP());
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # MonitoramentoPortaoTrilho
+
+<h1 id="usage" > 💻 Descrição </h1>
+
 Projeto que visa monitorar um portão automatico de trilho. É verificado se ele está aberto, fechado ou no meio do caminho. Se o portão fica aberto por muito tempo o usuario é notificado e pode fechalo remotamente.
+
+Componentes utilizados no projeto:
+- Um Esp32 Dev Module
+- Jumpers
+- Três sensores magneticos de porta
+- Três resistores de 10K Ohm
+
+Bibliotecas necessarias para Esp32:
+- Wifi
+- WiFiClient
+- PubSubClient
+- NTPClient
+
+<h2>Como rodar o projeto?</h2>
+
+É necessario que a IDE do arduino e as bibliotecas necessarias estejam instalados na maquina. Após instalar tudo, você deve escolher o modelo do seu Esp32 e a porta para fazer a gravação.
+
+
+<h3>Autores</h3>
+<a href="https://github.com/JoabUrbano">Joab Urbano</a><br>
+<a href="https://github.com/GabrielNSD">Gabriel Dantas</a><br>
+<a href="">João Pedro</a>
