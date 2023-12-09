38a8fed8d2d0becdf49043c55b31eb46cd762c01
Gabriel Dantas <gabrielnsdantas@gmail.com>
2023-12-09 12:14:24 -0300

    implementacao funcional do mqtt

--- MonitoramentoPortaoTrilho.ino
+++ MonitoramentoPortaoTrilho.ino
@@ -5,25 +5,25 @@
 #include <FS.h>
 #include <queue>
 
-#include <MQTTPubSubClient.h>
+#include <PubSubClient.h>
 
 std::queue<String> filaDeStrings;
 
 struct Button
 {
-  const uint8_t PIN;
-  bool pressed;
-  int buttonState;
-  int lastButtonState;
-  unsigned long int lastBounceTime;
+    const uint8_t PIN;
+    bool pressed;
+    int buttonState;
+    int lastButtonState;
+    unsigned long int lastBounceTime;
 };
 
 enum EstadoPortao
 {
-  ABERTO,
-  FECHADO,
-  MEIO,
-  MOVIMENTO
+    ABERTO,
+    FECHADO,
+    MEIO,
+    MOVIMENTO
 };
 
 enum EstadoPortao estadoPortao;
@@ -42,9 +42,9 @@ Button sensorFechado = {19, false, LOW, LOW, 0};
 int wifi_timeout = 100000;
 
 WiFiClient wifi_client;
-MQTTPubSubClient mqtt_client;
+PubSubClient client(wifi_client);
 
-#define mqtt_broker "homeassistant.local"
+const char *mqtt_broker = "homeassistant.local";
 const int mqtt_port = 1883;
 int mqtt_timeout = 10000;
 
@@ -94,97 +94,96 @@ void fecharPortao();
 void acaoControle(int botao);
 
 void tratarComandoMqtt(String comando);
+void callback(char *topic, byte *message, unsigned int length);
+
+void reconnect();
 
 // criar uma task para executar as acoes do portao em outro core
-
 TaskHandle_t Core0;
 
 void setup()
 {
-  pinMode(sensorAberto.PIN, INPUT);
-  pinMode(sensorMeio.PIN, INPUT);
-  pinMode(sensorFechado.PIN, INPUT);
-  attachInterrupt(sensorAberto.PIN, interrupcaoAberto, CHANGE);
-  attachInterrupt(sensorMeio.PIN, interrupcaoMeio, CHANGE);
-  attachInterrupt(sensorFechado.PIN, interrupcaoFechado, CHANGE);
+    pinMode(sensorAberto.PIN, INPUT);
+    pinMode(sensorMeio.PIN, INPUT);
+    pinMode(sensorFechado.PIN, INPUT);
+    attachInterrupt(sensorAberto.PIN, interrupcaoAberto, CHANGE);
+    attachInterrupt(sensorMeio.PIN, interrupcaoMeio, CHANGE);
+    attachInterrupt(sensorFechado.PIN, interrupcaoFechado, CHANGE);
 
-  pinMode(LED, OUTPUT);
-  pinMode(LED, OUTPUT);
-  pinMode(LEDFormat, OUTPUT);
-  pinMode(Format, INPUT);
-  Serial.begin(115200);
+    pinMode(LED, OUTPUT);
+    pinMode(LED, OUTPUT);
+    pinMode(LEDFormat, OUTPUT);
+    pinMode(Format, INPUT);
+    pinMode(botaoControleAbrir, OUTPUT);
+    pinMode(botaoControleFechar, OUTPUT);
+    Serial.begin(115200);
 
-  Timer0_Cfg = timerBegin(0, 80, true);
-  timerWrite(Timer0_Cfg, 0);
+    Timer0_Cfg = timerBegin(0, 80, true);
+    timerWrite(Timer0_Cfg, 0);
 
-  WiFi.mode(WIFI_STA); //"station mode": permite o ESP32 ser um cliente da rede WiFi
-  WiFi.begin(wifi_ssid, wifi_password);
-  connectWiFi();
+    WiFi.mode(WIFI_STA); //"station mode": permite o ESP32 ser um cliente da rede WiFi
+    WiFi.begin(wifi_ssid, wifi_password);
+    connectWiFi();
 
-  ntp.begin();       // Inicia o protocolo
-  ntp.forceUpdate(); // Atualização
+    ntp.begin();       // Inicia o protocolo
+    ntp.forceUpdate(); // Atualização
 
-  estadoPortao = FECHADO;
+    estadoPortao = FECHADO;
 
-  connectMQTT();
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
 
-  mqtt_client.subscribe("/mosquitto/portao/comandos", [](const String &payload, const size_t size)
-                        {
-        Serial.print("/mosquitto/portao/comandos ");
-        tratarComandoMqtt(payload); });
+    xTaskCreatePinnedToCore(
+        Task1Code,
+        "Task1",
+        10000,
+        NULL,
+        1,
+        &Core0,
+        0);
 
-  xTaskCreatePinnedToCore(
-      Task1Code,
-      "Task1",
-      10000,
-      NULL,
-      1,
-      &Core0,
-      0);
+    delay(500);
 
-  delay(500);
-
-  openFS();
+    openFS();
 }
 
 void Task1Code(void *parameter)
 {
-  for (;;)
-  {
-    if (comandoAbrir)
+    for (;;)
     {
-      abrirPortao();
-      comandoAbrir = false;
+
+        if (comandoAbrir)
+        {
+            abrirPortao();
+            comandoAbrir = false;
+        }
+        if (comandoFechar)
+        {
+            fecharPortao();
+            comandoFechar = false;
+        }
+        delay(100);
     }
-    if (comandoFechar)
-    {
-      fecharPortao();
-      comandoFechar = false;
-    }
-    delay(100);
-  }
 }
 
 void loop()
 {
-  /* Verifica o botão de formatação */
-  int val = digitalRead(Format);
-  if (val == HIGH)
-  {
-    digitalWrite(LEDFormat, HIGH);
-    Serial.println("Formatando Esp32");
-    formatFile();
-    digitalWrite(LEDFormat, LOW);
-    delay(1000);
-  }
+    /* Verifica o botão de formatação */
+    int val = digitalRead(Format);
+    if (val == HIGH)
+    {
+        digitalWrite(LEDFormat, HIGH);
+        Serial.println("Formatando Esp32");
+        formatFile();
+        digitalWrite(LEDFormat, LOW);
+        delay(1000);
+    }
 
-  if (!mqtt_connected)
-  {                // Se MQTT não estiver conectado
-    connectMQTT(); // Tente se conectar ao broker
-  }
-
-  if (mqtt_connected)
-  { // Se MQTT estiver conectado
+    if (!client.connected())
+    {                // Se MQTT não estiver conectado
+        reconnect(); // Tente se conectar ao broker
+    }
+    client.loop();
 
     // Executa ações de acordo com os sinais dos botões.
     acaoSensores();
@@ -193,240 +192,258 @@ void loop()
 
     if (tempoAberto > limiteTempoAberto)
     {
-      Serial.println("Portão aberto por mais de 60 segundos");
-      timerStop(Timer0_Cfg);
-      timerWrite(Timer0_Cfg, 0);
-      mqtt_client.update();
-      mqtt_client.publish("/mosquitto/portao/alarme", "Alarme ativado");
+        Serial.println("Portão aberto por mais de 60 segundos");
+        timerStop(Timer0_Cfg);
+        timerWrite(Timer0_Cfg, 0);
+        client.publish("/mosquitto/portao/alarme", "Alarme ativado");
     }
     delay(100);
-  }
 }
 
 void acaoSensores()
 {
-  if (sensorAberto.pressed && estadoPortao != ABERTO)
-  {
-    estadoPortao = ABERTO;
-    Serial.println("Aberto");
-    hora = ntp.getFormattedTime();
-    writeFile("Foi aberto", fileName, hora);
-    mqtt_client.update();
-    mqtt_client.publish("/mosquitto/portao/estadoSensores", "Aberto");
-  }
-  if (sensorMeio.pressed && estadoPortao != MEIO)
-  {
-    estadoPortao = MEIO;
-    Serial.println("Meio");
-    hora = ntp.getFormattedTime();
-    writeFile("Passou pelo meio", fileName, hora);
-    mqtt_client.update();
-    mqtt_client.publish("/mosquitto/portao/estadoSensores", "Meio");
-  }
-  if (sensorFechado.pressed && estadoPortao != FECHADO)
-  {
-    estadoPortao = FECHADO;
-    timerStop(Timer0_Cfg);
-    timerWrite(Timer0_Cfg, 0);
-    Serial.println("Fechado");
-    hora = ntp.getFormattedTime();
-    writeFile("Fechou", fileName, hora);
-    mqtt_client.update();
-    mqtt_client.publish("/mosquitto/portao/estadoSensores", "Fechado");
-  }
-  // se nenhum sersor está pressionado, o portão está em movimento
-  if (!sensorAberto.pressed && !sensorMeio.pressed && !sensorFechado.pressed && estadoPortao != MOVIMENTO)
-  {
-    estadoPortao = MOVIMENTO;
-    timerStart(Timer0_Cfg);
-  }
+    if (sensorAberto.pressed && estadoPortao != ABERTO)
+    {
+        estadoPortao = ABERTO;
+        Serial.println("Aberto");
+        hora = ntp.getFormattedTime();
+        writeFile("Foi aberto", fileName, hora);
+        client.publish("/mosquitto/portao/estadoSensores", "Aberto");
+    }
+    if (sensorMeio.pressed && estadoPortao != MEIO)
+    {
+        estadoPortao = MEIO;
+        Serial.println("Meio");
+        hora = ntp.getFormattedTime();
+        writeFile("Passou pelo meio", fileName, hora);
+        client.publish("/mosquitto/portao/estadoSensores", "Meio");
+    }
+    if (sensorFechado.pressed && estadoPortao != FECHADO)
+    {
+        estadoPortao = FECHADO;
+        timerStop(Timer0_Cfg);
+        timerWrite(Timer0_Cfg, 0);
+        Serial.println("Fechado");
+        hora = ntp.getFormattedTime();
+        writeFile("Fechou", fileName, hora);
+        client.publish("/mosquitto/portao/estadoSensores", "Fechado");
+    }
+    // se nenhum sersor está pressionado, o portão está em movimento
+    if (!sensorAberto.pressed && !sensorMeio.pressed && !sensorFechado.pressed && estadoPortao != MOVIMENTO)
+    {
+        estadoPortao = MOVIMENTO;
+        timerStart(Timer0_Cfg);
+    }
 };
 
 // Função para conectar o Esp ao Wifi
 void connectWiFi()
 {
-  Serial.print("Conectando à rede WiFi .. ");
+    Serial.print("Conectando à rede WiFi .. ");
 
-  unsigned long tempoInicial = millis();
-  while (WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout))
-  {
-    Serial.print(".");
-    delay(100);
-  }
-  Serial.println();
+    unsigned long tempoInicial = millis();
+    while (WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout))
+    {
+        Serial.print(".");
+        delay(100);
+    }
+    Serial.println();
 
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("Conexão com WiFi falhou!");
-    digitalWrite(LED, LOW);
-  }
-  else
-  {
-    Serial.print("Conectado com o IP: ");
-    Serial.println(WiFi.localIP());
-    digitalWrite(LED, HIGH);
-  }
-}
-
-void connectMQTT()
-{
-  Serial.print("connecting to host...");
-  while (!wifi_client.connect(mqtt_broker, 1883))
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println(" connected!");
-
-  mqtt_client.begin(wifi_client);
-  unsigned long tempoInicial = millis();
-  while (!mqtt_client.connect("Esp32-portao", "mosquitto", "mosquitto"))
-  {
-    Serial.print(".");
-    delay(500);
-  }
-  mqtt_connected = true;
-  Serial.println();
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.println("Conexão com WiFi falhou!");
+        digitalWrite(LED, LOW);
+    }
+    else
+    {
+        Serial.print("Conectado com o IP: ");
+        Serial.println(WiFi.localIP());
+        digitalWrite(LED, HIGH);
+    }
 }
 
 void abrirPortao()
 {
-  Serial.println("Abrindo portão");
-  acaoControle(botaoControleAbrir);
+    Serial.println("Abrindo portão");
+    acaoControle(botaoControleAbrir);
 };
 
 void fecharPortao()
 {
-  acaoControle(botaoControleFechar);
+    Serial.println("Fechando portão");
+    acaoControle(botaoControleFechar);
 };
 
 void acaoControle(int botao)
 {
-  digitalWrite(botao, HIGH);
-  delay(500);
-  digitalWrite(botao, LOW);
+    digitalWrite(botao, HIGH);
+    delay(500);
+    digitalWrite(botao, LOW);
 };
 
 void tratarComandoMqtt(String comando)
 {
-  if (comando == "abrir")
-  {
-    comandoAbrir = true;
-  }
-  if (comando == "fechar")
-  {
-    comandoFechar = true;
-  }
+    Serial.println("Comando recebido: " + comando);
+    if (comando == "abrir")
+    {
+        comandoAbrir = true;
+    }
+    if (comando == "fechar")
+    {
+        comandoFechar = true;
+    }
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+    Serial.print("Mensagem Recebida no topic: ");
+    Serial.print(topic);
+    Serial.print(". Mensagem: ");
+    String messageTemp;
+
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)message[i]);
+        messageTemp += (char)message[i];
+    }
+    Serial.println();
+
+    if (String(topic) == "/mosquitto/portao/comandos")
+    {
+        tratarComandoMqtt(messageTemp);
+    }
+}
+
+void reconnect()
+{
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect("ESP32Client", "mosquitto", "mosquitto"))
+        {
+            Serial.println("connected");
+            // Subscribe
+            client.subscribe("/mosquitto/portao/comandos");
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 0.5 seconds");
+            delay(500);
+        }
+    }
 }
 
 // Declaração das funçoẽs de interrupção
 void IRAM_ATTR interrupcaoFechado()
 {
-  debounceBotao(&sensorFechado);
+    debounceBotao(&sensorFechado);
 };
 void IRAM_ATTR interrupcaoMeio()
 {
-  debounceBotao(&sensorMeio);
+    debounceBotao(&sensorMeio);
 };
 void IRAM_ATTR interrupcaoAberto()
 {
-  debounceBotao(&sensorAberto);
+    debounceBotao(&sensorAberto);
 };
 
 void debounceBotao(Button *button)
 {
-  button->buttonState = digitalRead(button->PIN);
-  // O debounce só acontece se o botão mudar de estado
-  if ((millis() - button->lastBounceTime > debounceDelay) && button->buttonState != button->lastButtonState)
-  {
-    if (button->buttonState == HIGH)
+    button->buttonState = digitalRead(button->PIN);
+    // O debounce só acontece se o botão mudar de estado
+    if ((millis() - button->lastBounceTime > debounceDelay) && button->buttonState != button->lastButtonState)
     {
-      button->pressed = true;
-      button->lastButtonState = HIGH;
+        if (button->buttonState == HIGH)
+        {
+            button->pressed = true;
+            button->lastButtonState = HIGH;
+        }
+        else
+        {
+            button->lastButtonState = LOW;
+            button->pressed = false;
+        }
+        button->lastBounceTime = millis();
     }
-    else
-    {
-      button->lastButtonState = LOW;
-      button->pressed = false;
-    }
-    button->lastBounceTime = millis();
-  }
 }
 
 /* Funções de interação arquivos ESP */
 
 void writeFile(String state, String path, String hora)
 {
-  while (!filaDeStrings.empty())
-  {
-    filaDeStrings.pop();
-  }
+    while (!filaDeStrings.empty())
+    {
+        filaDeStrings.pop();
+    }
 
-  readFile(path);
-  File rFile = SPIFFS.open(path, "w");
+    readFile(path);
+    File rFile = SPIFFS.open(path, "w");
 
-  if (!rFile)
-  {
-    Serial.println("Erro ao abrir arquivo!");
-    return;
-  }
-  filaDeStrings.push(state + "," + hora);
+    if (!rFile)
+    {
+        Serial.println("Erro ao abrir arquivo!");
+        return;
+    }
+    filaDeStrings.push(state + "," + hora);
 
-  // Escrever no arquivo
-  while (filaDeStrings.size() > maxLines)
-  {
-    filaDeStrings.pop();
-  }
-  while (!filaDeStrings.empty())
-  {
-    rFile.println(filaDeStrings.front());
-    filaDeStrings.pop();
-  }
+    // Escrever no arquivo
+    while (filaDeStrings.size() > maxLines)
+    {
+        filaDeStrings.pop();
+    }
+    while (!filaDeStrings.empty())
+    {
+        rFile.println(filaDeStrings.front());
+        filaDeStrings.pop();
+    }
 
-  rFile.close();
+    rFile.close();
 }
 
 void readFile(String path)
 {
-  Serial.println("Read file");
+    Serial.println("Read file");
 
-  File rFile = SPIFFS.open(path, "r"); // r+ leitura e escrita
+    File rFile = SPIFFS.open(path, "r"); // r+ leitura e escrita
 
-  if (!rFile)
-  {
-    Serial.println("Erro ao abrir arquivo!");
-
-    return;
-  }
-
-  else
-  {
-    Serial.print("---------- Lendo arquivo ");
-
-    Serial.print(path);
-
-    Serial.println("  ---------");
-    while (rFile.position() < rFile.size())
+    if (!rFile)
     {
-      String line = rFile.readStringUntil('\n'); // Lê uma linha do arquivo
-      Serial.println(line);
-      filaDeStrings.push(line);
+        Serial.println("Erro ao abrir arquivo!");
+
+        return;
     }
 
-    rFile.close();
-  }
+    else
+    {
+        Serial.print("---------- Lendo arquivo ");
+
+        Serial.print(path);
+
+        Serial.println("  ---------");
+        while (rFile.position() < rFile.size())
+        {
+            String line = rFile.readStringUntil('\n'); // Lê uma linha do arquivo
+            Serial.println(line);
+            filaDeStrings.push(line);
+        }
+
+        rFile.close();
+    }
 }
 
 void formatFile()
 {
-  SPIFFS.format();
-  Serial.println("Formatou SPIFFS");
+    SPIFFS.format();
+    Serial.println("Formatou SPIFFS");
 }
 
 void openFS(void)
 {
-  if (!SPIFFS.begin())
-    Serial.println("\nErro ao abrir o sistema de arquivos");
-  else
-    Serial.println("\nSistema de arquivos aberto com sucesso!");
+    if (!SPIFFS.begin())
+        Serial.println("\nErro ao abrir o sistema de arquivos");
+    else
+        Serial.println("\nSistema de arquivos aberto com sucesso!");
 }
